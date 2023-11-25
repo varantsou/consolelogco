3d8be8285e6de955a053f624617812d5241875f6
vvorontsov <valery.varantsou@carrier.com>
2023-11-25 21:19:49 +0100

    test

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "nextamplified": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
